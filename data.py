ad8ff1dd91a8c035e15f4a48276ec3c6f3f96b81
Casey <87348212+2cheinz@users.noreply.github.com>
2025-04-29 14:15:10 -0500

    load team data via hardcoded data.py file

---
+++ data.py
@@ -0,0 +1,18 @@
+# load the NFL teams that we will be using for testing, hardcoded for now
+
+AFC_TEAMS = [
+    "Baltimore Ravens", "Buffalo Bills", "Cincinnati Bengals", "Cleveland Browns",
+    "Denver Broncos", "Houston Texans", "Indianapolis Colts", "Jacksonville Jaguars",
+    "Kansas City Chiefs", "Las Vegas Raiders", "Los Angeles Chargers", "Miami Dolphins",
+    "New England Patriots", "New York Jets", "Pittsburgh Steelers", "Tennessee Titans"
+]
+
+NFC_TEAMS = [
+    "Arizona Cardinals", "Atlanta Falcons", "Carolina Panthers", "Chicago Bears",
+    "Dallas Cowboys", "Detroit Lions", "Green Bay Packers", "Los Angeles Rams",
+    "Minnesota Vikings", "New Orleans Saints", "New York Giants", "Philadelphia Eagles",
+    "San Francisco 49ers", "Seattle Seahawks", "Tampa Bay Buccaneers", "Washington Commanders"
+]
+
+# favorite list that will adjust accordingly to actions taken by the user
+favorites = []
