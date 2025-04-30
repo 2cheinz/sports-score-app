cc1ce3f0fd485936b988903e5c372e90f45ade41
Casey <87348212+2cheinz@users.noreply.github.com>
2025-04-30 08:49:30 -0500

    template for main.py main menu page created

---
+++ main.py
@@ -0,0 +1,25 @@
+# main file for running
+
+def display_main_menu():
+    print("\nWelcome to the Sports Team Tracker")
+    print("1. Search for Teams")
+    print("2. View Favorite Teams")
+    print("3. Exit Program")
+
+def main():
+    while True:
+        display_main_menu()
+        choice = input("Enter your choice (1-3): ")
+
+        if choice == "1":
+            search_teams_menu()
+        elif choice == "2":
+            view_favorites()
+        elif choice == "3":
+            print("Thanks for using the Sports Score Tracker, have a good day!")
+            break
+        else:
+            print("Invalid choice. Please enter a number between 1 and 3.")
+
+if __name__ == "__main__":
+    main()
